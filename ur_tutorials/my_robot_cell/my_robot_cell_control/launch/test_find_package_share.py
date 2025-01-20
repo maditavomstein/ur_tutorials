ffe2b7a721fff654cc0a0926b9e19e60aa3de3f5
maditavomstein <maditavomstein@gmail.com>
2025-01-20 20:14:04 +0100

    Ordner bereinigt von Test- und nicht genutzten Dateien

--- ur_tutorials/my_robot_cell/my_robot_cell_control/launch/test_find_package_share.py
+++
@@ -1,6 +0,0 @@
-from launch_ros.substitutions import FindPackageShare
-from launch import LaunchContext
-
-context = LaunchContext()
-substitution = FindPackageShare("my_robot_cell_control")
-print(substitution.perform(context))
